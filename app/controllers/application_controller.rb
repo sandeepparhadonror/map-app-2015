2c113ef1d1fc1c2e1257fdfd256be07b4986a131
sandeep parhad <sandeepparh@cybage.com>
2015-06-04 16:27:22 +0530

    revert the changes to sign in and up and timeout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,25 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  #before_filter :authenticate_user!
-  helper_method :current_user
-
-  def current_user
-    super || guest_user
-  end
-
-  private
-
-  def guest_user
-    User.find(session[:guest_user_id].nil? ? session[:guest_user_id] = create_guest_user.id : session[:guest_user_id])
-  end
   
-  def create_guest_user
-    user = User.new { |user| user.guest = true }
-    user.email = "guest_#{Time.now.to_i}#{rand(99)}@example.com"
-    user.save(:validate => false)
-    user
-  end
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603114804) do
+ActiveRecord::Schema.define(version: 20150602073939) do
 
   create_table "locations", force: true do |t|
     t.float    "latitude"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 20150603114804) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "guest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
